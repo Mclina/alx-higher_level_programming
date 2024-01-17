fb1db8d1eb2b2db11e5f13a230c7b473478222ec
Mclina <mariacelinoh@gmail.com>
2024-01-17 09:47:16 -0800

    6-print_comb3.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-# prints numbers from 0 to 98 separated by , .
-for i in range(100):
-    print('{:02d}'.format(i), end=", " if i < 99 else '\n')
+
+for i in range(0, 8):
+    for j in range(0, 10):
+        if i < j:
+            print("{:d}{:d}, ".format(i, j), end="")
+print("89")
