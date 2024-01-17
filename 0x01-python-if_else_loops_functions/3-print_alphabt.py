8eb9c76e68fa9ad7766728577e42a29f15d634e0
Mclina <mariacelinoh@gmail.com>
2024-01-17 09:50:47 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-# prints the ASCII alphabet, in lowercase, not followed by a new line.
-# ASCII code of the character codes for 'a' and 'z' is between 97 and 122.
-for alpha_letters in range(97, 123):
-    if alpha_letters == 101 or alpha_letters == 113:
-        continue
-    print("{}".format(chr(alpha_letters)), end="")
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{:c}".format(i), end="")
