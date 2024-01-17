f8ce997d37a899ef24a04506352b344744413505
Mclina <mariacelinoh@gmail.com>
2024-01-17 09:57:35 -0800

    lists.h

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -1,13 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
+#include <stddef.h>
+
 /**
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- *
+ * for Holberton project
  */
 typedef struct listint_s
 {
